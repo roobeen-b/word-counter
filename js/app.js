e8011da1e72dbb9dd87f80db7d160b102a3b2bd3
roobeen-b <rubinbaidhya@gmail.com>
2022-01-16 19:51:36 +0545

    Final word counter project

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const inputText = document.querySelector("#text");
+const statElem = document.querySelector("#stat");
+
+new wordCounter(inputText);
+
+const render = (event) => {
+  statElem.innerHTML = `<p>You have written <span class="highlight">${event.detail.wordStat.words} words</span>
+    and <span class="highlight">${event.detail.wordStat.characters} characters</span>.</p>`;
+};
+
+inputText.addEventListener("count", render);
